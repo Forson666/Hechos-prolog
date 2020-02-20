4ece15f6bfa3aa8a435f9f4a694ae8fafa20a562
Forson666 <51457747+Forson666@users.noreply.github.com>
2020-02-20 13:44:31 -0500

    Add files via upload

---
+++ pasa la materia.pl
@@ -0,0 +1,133 @@
+inscrito("Juan","Modelos 2").
+inscrito("Jorge","Modelos 2").
+inscrito("Carlos","Modelos 2").
+inscrito("Elkin","Modelos 2").
+inscrito("Goku","Modelos 2").
+inscrito("Jose","Modelos 2").
+inscrito("Nayibe","Modelos 2").
+inscrito("Yuyeimi","Modelos 2").
+inscrito("Yo","Modelos 2").
+inscrito("Lenon","Modelos 2").
+
+definitiva("Juan","Modelos 2",30).
+definitiva("Jorge","Modelos 2",40).
+definitiva("Carlos","Modelos 2",25).
+definitiva("Elkin","Modelos 2",13).
+definitiva("Goku","Modelos 2",33).
+definitiva("Jose","Modelos 2",44).
+definitiva("Nayibe","Modelos 2",20).
+definitiva("Yuyeimi","Modelos 2",30).
+definitiva("Yo","Modelos 2",5).
+definitiva("Lenon","Modelos 2",45).
+
+numerodeclases("Modelos 2", 100).
+
+fallas("Juan","Modelos 2",31).
+fallas("Jorge","Modelos 2",2).
+fallas("Carlos","Modelos 2",15).
+fallas("Elkin","Modelos 2",1).
+fallas("Goku","Modelos 2",3).
+fallas("Jose","Modelos 2",4).
+fallas("Nayibe","Modelos 2",0).
+fallas("Yuyeimi","Modelos 2",3).
+fallas("Yo","Modelos 2",0).
+fallas("Lenon","Modelos 2",5).
+
+pasa("Jorge","Modelos 2").
+pasa("Goku","Modelos 2").
+pasa("Jose","Modelos 2").
+pasa("Yuyeimi","Modelos 2").
+pasa("Lenon","Modelos 2").
+
+inscrito("Luis","Modelos 1").
+inscrito("Ricardo","Modelos 1").
+inscrito("Eddy","Modelos 1").
+inscrito("Ed","Modelos 1").
+inscrito("DD","Modelos 1").
+inscrito("Dexter","Modelos 1").
+inscrito("Timmy","Modelos 1").
+inscrito("Cosmo","Modelos 1").
+inscrito("Dave","Modelos 1").
+inscrito("Siri","Modelos 1").
+
+definitiva("Luis","Modelos 1",24).
+definitiva("Ricardo","Modelos 1",43).
+definitiva("Eddy","Modelos 1",33).
+definitiva("Ed","Modelos 1",10).
+definitiva("DD","Modelos 1",50).
+definitiva("Dexter","Modelos 1",50).
+definitiva("Timmy","Modelos 1",29).
+definitiva("Cosmo","Modelos 1",15).
+definitiva("Dave","Modelos 1",22).
+definitiva("Siri","Modelos 1",49).
+
+numerodeclases("Modelos 1", 100).
+
+fallas("Luis","Modelos 1",1).
+fallas("Ricardo","Modelos 1",21).
+fallas("Eddy","Modelos 1",20).
+fallas("Ed","Modelos 1",31).
+fallas("DD","Modelos 1",0).
+fallas("Dexter","Modelos 1",0).
+fallas("Timmy","Modelos 1",54).
+fallas("Cosmo","Modelos 1",55).
+fallas("Dave","Modelos 1",2).
+fallas("Siri","Modelos 1",0).
+
+pasa("Ricardo","Modelos 1").
+pasa("Eddy","Modelos 1").
+pasa("DD","Modelos 1").
+pasa("Dexter","Modelos 1").
+pasa("Siri","Modelos 1").
+
+inscrito("Luis","Gestion de tecnologia").
+inscrito("Ricardo","Gestion de tecnologia").
+inscrito("PEPE","Gestion de tecnologia").
+inscrito("Brayan","Gestion de tecnologia").
+inscrito("Saul","Gestion de tecnologia").
+inscrito("Dexter","Gestion de tecnologia").
+inscrito("Timmy","Gestion de tecnologia").
+inscrito("Cosmo","Gestion de tecnologia").
+inscrito("Dave","Gestion de tecnologia").
+inscrito("Siri","Gestion de tecnologia").
+
+definitiva("Luis","Gestion de tecnologia",24).
+definitiva("Ricardo","Gestion de tecnologia",43).
+definitiva("Eddy","Gestion de tecnologia",33).
+definitiva("Ed","Gestion de tecnologia",10).
+definitiva("DD","Gestion de tecnologia",50).
+definitiva("Dexter","Gestion de tecnologia",50).
+definitiva("Timmy","Gestion de tecnologia",29).
+definitiva("Cosmo","Gestion de tecnologia",15).
+definitiva("Dave","Gestion de tecnologia",22).
+definitiva("Siri","Gestion de tecnologia",49).
+
+numerodeclases("Gestion de tecnologia", 100).
+
+fallas("Luis","Gestion de tecnologia",1).
+fallas("Ricardo","Gestion de tecnologia",21).
+fallas("Eddy","Gestion de tecnologia",20).
+fallas("Ed","Gestion de tecnologia",31).
+fallas("DD","Gestion de tecnologia",0).
+fallas("Dexter","Gestion de tecnologia",0).
+fallas("Timmy","Gestion de tecnologia",54).
+fallas("Cosmo","Gestion de tecnologia",55).
+fallas("Dave","Gestion de tecnologia",2).
+fallas("Siri","Gestion de tecnologia",0).
+
+pasa("Ricardo","Gestion de tecnologia").
+pasa("Eddy","Gestion de tecnologia").
+pasa("DD","Gestion de tecnologia").
+pasa("Dexter","Gestion de tecnologia").
+pasa("Siri","Gestion de tecnologia").
+
+
+
+
+
+
+
+
+
+
+
