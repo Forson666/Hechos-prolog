fddc48d4a52f54cbdf573d6833ed1bc9f0276ac9
iMrJulian <36264252+iMrJulian@users.noreply.github.com>
2020-02-26 21:59:47 -0500

    Add files via upload

--- pasa.pl
+++ pasa.pl
@@ -77,172 +77,172 @@ fallas("Daniel","Gestion de tecnologia",31).
 %pasa("Lenon","Modelos 2").
 %
 
-nota1("Juan","Modelos 2","Taller 1",30,15).
-nota1("Jorge","Modelos 2","Taller 1",40,15).
-nota1("Carlos","Modelos 2","Taller 1",25,15).
-nota1("Elkin","Modelos 2","Taller 1",13,15).
-nota1("Goku","Modelos 2","Taller 1",33,15).
-nota1("Jose","Modelos 2","Taller 1",44,15).
-nota1("Nayibe","Modelos 2","Taller 1",20,15).
-nota1("Yuyeimi","Modelos 2","Taller 1",30,15).
-nota1("Yo","Modelos 2","Taller 1",50,15).
-nota1("Lenon","Modelos 2","Taller 1",45,15).
+nota("Juan","Modelos 2","Taller 1",30,15).
+nota("Jorge","Modelos 2","Taller 1",40,15).
+nota("Carlos","Modelos 2","Taller 1",25,15).
+nota("Elkin","Modelos 2","Taller 1",13,15).
+nota("Goku","Modelos 2","Taller 1",33,15).
+nota("Jose","Modelos 2","Taller 1",44,15).
+nota("Nayibe","Modelos 2","Taller 1",20,15).
+nota("Yuyeimi","Modelos 2","Taller 1",30,15).
+nota("Yo","Modelos 2","Taller 1",50,15).
+nota("Lenon","Modelos 2","Taller 1",45,15).
 
-nota1("Luis","Modelos 1","Taller 1",24,7).
-nota1("Ricardo","Modelos 1","Taller 1",43,7).
-nota1("Eddy","Modelos 1","Taller 1",33,7).
-nota1("Ed","Modelos 1","Taller 1",10,7).
-nota1("DD","Modelos 1","Taller 1",50,7).
-nota1("Dexter","Modelos 1","Taller 1",50,7).
-nota1("Timmy","Modelos 1","Taller 1",29,7).
-nota1("Cosmo","Modelos 1","Taller 1",15,7).
-nota1("Dave","Modelos 1","Taller 1",22,7).
-nota1("Siri","Modelos 1","Taller 1",49,7).
+nota("Luis","Modelos 1","Taller 1",24,7).
+nota("Ricardo","Modelos 1","Taller 1",43,7).
+nota("Eddy","Modelos 1","Taller 1",33,7).
+nota("Ed","Modelos 1","Taller 1",10,7).
+nota("DD","Modelos 1","Taller 1",50,7).
+nota("Dexter","Modelos 1","Taller 1",50,7).
+nota("Timmy","Modelos 1","Taller 1",29,7).
+nota("Cosmo","Modelos 1","Taller 1",15,7).
+nota("Dave","Modelos 1","Taller 1",22,7).
+nota("Siri","Modelos 1","Taller 1",49,7).
 
-nota1("Josue","Gestion de tecnologia","Taller 1",35,10).
-nota1("David","Gestion de tecnologia","Taller 1",46,10).
-nota1("Andres","Gestion de tecnologia","Taller 1",32,10).
-nota1("Liliana","Gestion de tecnologia","Taller 1",50,10).
-nota1("Sofia","Gestion de tecnologia","Taller 1",42,10).
-nota1("Carmen","Gestion de tecnologia","Taller 1",31,10).
-nota1("Charle","Gestion de tecnologia","Taller 1",47,10).
-nota1("Manuel","Gestion de tecnologia","Taller 1",21,10).
-nota1("Eduardo","Gestion de tecnologia","Taller 1",11,10).
-nota1("Daniel","Gestion de tecnologia","Taller 1",30,10).
+nota("Josue","Gestion de tecnologia","Taller 1",35,10).
+nota("David","Gestion de tecnologia","Taller 1",46,10).
+nota("Andres","Gestion de tecnologia","Taller 1",32,10).
+nota("Liliana","Gestion de tecnologia","Taller 1",50,10).
+nota("Sofia","Gestion de tecnologia","Taller 1",42,10).
+nota("Carmen","Gestion de tecnologia","Taller 1",31,10).
+nota("Charle","Gestion de tecnologia","Taller 1",47,10).
+nota("Manuel","Gestion de tecnologia","Taller 1",21,10).
+nota("Eduardo","Gestion de tecnologia","Taller 1",11,10).
+nota("Daniel","Gestion de tecnologia","Taller 1",30,10).
 
-nota2("Juan","Modelos 2","Taller 2",30,15).
-nota2("Jorge","Modelos 2","Taller 2",40,15).
-nota2("Carlos","Modelos 2","Taller 2",25,15).
-nota2("Elkin","Modelos 2","Taller 2",13,15).
-nota2("Goku","Modelos 2","Taller 2",33,15).
-nota2("Jose","Modelos 2","Taller 2",44,15).
-nota2("Nayibe","Modelos 2","Taller 2",20,15).
-nota2("Yuyeimi","Modelos 2","Taller 2",30,15).
-nota2("Yo","Modelos 2","Taller 2",50,15).
-nota2("Lenon","Modelos 2","Taller 2",45,15).
+nota("Juan","Modelos 2","Taller 2",30,15).
+nota("Jorge","Modelos 2","Taller 2",40,15).
+nota("Carlos","Modelos 2","Taller 2",25,15).
+nota("Elkin","Modelos 2","Taller 2",13,15).
+nota("Goku","Modelos 2","Taller 2",33,15).
+nota("Jose","Modelos 2","Taller 2",44,15).
+nota("Nayibe","Modelos 2","Taller 2",20,15).
+nota("Yuyeimi","Modelos 2","Taller 2",30,15).
+nota("Yo","Modelos 2","Taller 2",50,15).
+nota("Lenon","Modelos 2","Taller 2",45,15).
 
-nota2("Luis","Modelos 1","Taller 2",24,13).
-nota2("Ricardo","Modelos 1","Taller 2",43,13).
-nota2("Eddy","Modelos 1","Taller 2",33,13).
-nota2("Ed","Modelos 1","Taller 2",10,13).
-nota2("DD","Modelos 1","Taller 2",50,13).
-nota2("Dexter","Modelos 1","Taller 2",50,13).
-nota2("Timmy","Modelos 1","Taller 2",29,13).
-nota2("Cosmo","Modelos 1","Taller 2",15,13).
-nota2("Dave","Modelos 1","Taller 2",22,13).
-nota2("Siri","Modelos 1","Taller 2",49,13).
+nota("Luis","Modelos 1","Taller 2",24,13).
+nota("Ricardo","Modelos 1","Taller 2",43,13).
+nota("Eddy","Modelos 1","Taller 2",33,13).
+nota("Ed","Modelos 1","Taller 2",10,13).
+nota("DD","Modelos 1","Taller 2",50,13).
+nota("Dexter","Modelos 1","Taller 2",50,13).
+nota("Timmy","Modelos 1","Taller 2",29,13).
+nota("Cosmo","Modelos 1","Taller 2",15,13).
+nota("Dave","Modelos 1","Taller 2",22,13).
+nota("Siri","Modelos 1","Taller 2",49,13).
 
-nota2("Josue","Gestion de tecnologia","Taller 2",35,15).
-nota2("David","Gestion de tecnologia","Taller 2",46,15).
-nota2("Andres","Gestion de tecnologia","Taller 2",32,15).
-nota2("Liliana","Gestion de tecnologia","Taller 2",50,15).
-nota2("Sofia","Gestion de tecnologia","Taller 2",42,15).
-nota2("Carmen","Gestion de tecnologia","Taller 2",31,15).
-nota2("Charle","Gestion de tecnologia","Taller 2",47,15).
-nota2("Manuel","Gestion de tecnologia","Taller 2",21,15).
-nota2("Eduardo","Gestion de tecnologia","Taller 2",11,15).
-nota2("Daniel","Gestion de tecnologia","Taller 2",30,15).
+nota("Josue","Gestion de tecnologia","Taller 2",35,15).
+nota("David","Gestion de tecnologia","Taller 2",46,15).
+nota("Andres","Gestion de tecnologia","Taller 2",32,15).
+nota("Liliana","Gestion de tecnologia","Taller 2",50,15).
+nota("Sofia","Gestion de tecnologia","Taller 2",42,15).
+nota("Carmen","Gestion de tecnologia","Taller 2",31,15).
+nota("Charle","Gestion de tecnologia","Taller 2",47,15).
+nota("Manuel","Gestion de tecnologia","Taller 2",21,15).
+nota("Eduardo","Gestion de tecnologia","Taller 2",11,15).
+nota("Daniel","Gestion de tecnologia","Taller 2",30,15).
 
-nota3("Juan","Modelos 2","Parcial 1",30,20).
-nota3("Jorge","Modelos 2","Parcial 1",40,20).
-nota3("Carlos","Modelos 2","Parcial 1",25,20).
-nota3("Elkin","Modelos 2","Parcial 1",13,20).
-nota3("Goku","Modelos 2","Parcial 1",33,20).
-nota3("Jose","Modelos 2","Parcial 1",44,20).
-nota3("Nayibe","Modelos 2","Parcial 1",20,20).
-nota3("Yuyeimi","Modelos 2","Parcial 1",30,20).
-nota3("Yo","Modelos 2","Parcial 1",50,20).
-nota3("Lenon","Modelos 2","Parcial 1",45,20).
+nota("Juan","Modelos 2","Parcial 1",30,20).
+nota("Jorge","Modelos 2","Parcial 1",40,20).
+nota("Carlos","Modelos 2","Parcial 1",25,20).
+nota("Elkin","Modelos 2","Parcial 1",13,20).
+nota("Goku","Modelos 2","Parcial 1",33,20).
+nota("Jose","Modelos 2","Parcial 1",44,20).
+nota("Nayibe","Modelos 2","Parcial 1",20,20).
+nota("Yuyeimi","Modelos 2","Parcial 1",30,20).
+nota("Yo","Modelos 2","Parcial 1",50,20).
+nota("Lenon","Modelos 2","Parcial 1",45,20).
 
-nota3("Luis","Modelos 1","Parcial 1",24,25).
-nota3("Ricardo","Modelos 1","Parcial 1",43,25).
-nota3("Eddy","Modelos 1","Parcial 1",33,25).
-nota3("Ed","Modelos 1","Parcial 1",10,25).
-nota3("DD","Modelos 1","Parcial 1",50,25).
-nota3("Dexter","Modelos 1","Parcial 1",50,25).
-nota3("Timmy","Modelos 1","Parcial 1",29,25).
-nota3("Cosmo","Modelos 1","Parcial 1",15,25).
-nota3("Dave","Modelos 1","Parcial 1",22,25).
-nota3("Siri","Modelos 1","Parcial 1",49,25).
+nota("Luis","Modelos 1","Parcial 1",24,25).
+nota("Ricardo","Modelos 1","Parcial 1",43,25).
+nota("Eddy","Modelos 1","Parcial 1",33,25).
+nota("Ed","Modelos 1","Parcial 1",10,25).
+nota("DD","Modelos 1","Parcial 1",50,25).
+nota("Dexter","Modelos 1","Parcial 1",50,25).
+nota("Timmy","Modelos 1","Parcial 1",29,25).
+nota("Cosmo","Modelos 1","Parcial 1",15,25).
+nota("Dave","Modelos 1","Parcial 1",22,25).
+nota("Siri","Modelos 1","Parcial 1",49,25).
 
-nota3("Josue","Gestion de tecnologia","Parcial 1",35,25).
-nota3("David","Gestion de tecnologia","Parcial 1",46,25).
-nota3("Andres","Gestion de tecnologia","Parcial 1",32,25).
-nota3("Liliana","Gestion de tecnologia","Parcial 1",50,25).
-nota3("Sofia","Gestion de tecnologia","Parcial 1",42,25).
-nota3("Carmen","Gestion de tecnologia","Parcial 1",31,25).
-nota3("Charle","Gestion de tecnologia","Parcial 1",47,25).
-nota3("Manuel","Gestion de tecnologia","Parcial 1",21,25).
-nota3("Eduardo","Gestion de tecnologia","Parcial 1",11,25).
-nota3("Daniel","Gestion de tecnologia","Parcial 1",30,25).
+nota("Josue","Gestion de tecnologia","Parcial 1",35,25).
+nota("David","Gestion de tecnologia","Parcial 1",46,25).
+nota("Andres","Gestion de tecnologia","Parcial 1",32,25).
+nota("Liliana","Gestion de tecnologia","Parcial 1",50,25).
+nota("Sofia","Gestion de tecnologia","Parcial 1",42,25).
+nota("Carmen","Gestion de tecnologia","Parcial 1",31,25).
+nota("Charle","Gestion de tecnologia","Parcial 1",47,25).
+nota("Manuel","Gestion de tecnologia","Parcial 1",21,25).
+nota("Eduardo","Gestion de tecnologia","Parcial 1",11,25).
+nota("Daniel","Gestion de tecnologia","Parcial 1",30,25).
 
-nota4("Juan","Modelos 2","Parcial 2",30,20).
-nota4("Jorge","Modelos 2","Parcial 2",40,20).
-nota4("Carlos","Modelos 2","Parcial 2",25,20).
-nota4("Elkin","Modelos 2","Parcial 2",13,20).
-nota4("Goku","Modelos 2","Parcial 2",33,20).
-nota4("Jose","Modelos 2","Parcial 2",44,20).
-nota4("Nayibe","Modelos 2","Parcial 2",20,20).
-nota4("Yuyeimi","Modelos 2","Parcial 2",30,20).
-nota4("Yo","Modelos 2","Parcial 2",50,20).
-nota4("Lenon","Modelos 2","Parcial 2",45,20).
+nota("Juan","Modelos 2","Parcial 2",30,20).
+nota("Jorge","Modelos 2","Parcial 2",40,20).
+nota("Carlos","Modelos 2","Parcial 2",25,20).
+nota("Elkin","Modelos 2","Parcial 2",13,20).
+nota("Goku","Modelos 2","Parcial 2",33,20).
+nota("Jose","Modelos 2","Parcial 2",44,20).
+nota("Nayibe","Modelos 2","Parcial 2",20,20).
+nota("Yuyeimi","Modelos 2","Parcial 2",30,20).
+nota("Yo","Modelos 2","Parcial 2",50,20).
+nota("Lenon","Modelos 2","Parcial 2",45,20).
 
-nota4("Luis","Modelos 1","Parcial 2",24,25).
-nota4("Ricardo","Modelos 1","Parcial 2",43,25).
-nota4("Eddy","Modelos 1","Parcial 2",33,25).
-nota4("Ed","Modelos 1","Parcial 2",10,25).
-nota4("DD","Modelos 1","Parcial 2",50,25).
-nota4("Dexter","Modelos 1","Parcial 2",50,25).
-nota4("Timmy","Modelos 1","Parcial 2",29,25).
-nota4("Cosmo","Modelos 1","Parcial 2",15,25).
-nota4("Dave","Modelos 1","Parcial 2",22,25).
-nota4("Siri","Modelos 1","Parcial 2",49,25).
+nota("Luis","Modelos 1","Parcial 2",24,25).
+nota("Ricardo","Modelos 1","Parcial 2",43,25).
+nota("Eddy","Modelos 1","Parcial 2",33,25).
+nota("Ed","Modelos 1","Parcial 2",10,25).
+nota("DD","Modelos 1","Parcial 2",50,25).
+nota("Dexter","Modelos 1","Parcial 2",50,25).
+nota("Timmy","Modelos 1","Parcial 2",29,25).
+nota("Cosmo","Modelos 1","Parcial 2",15,25).
+nota("Dave","Modelos 1","Parcial 2",22,25).
+nota("Siri","Modelos 1","Parcial 2",49,25).
 
-nota4("Josue","Gestion de tecnologia","Parcial 2",35,20).
-nota4("David","Gestion de tecnologia","Parcial 2",46,20).
-nota4("Andres","Gestion de tecnologia","Parcial 2",32,20).
-nota4("Liliana","Gestion de tecnologia","Parcial 2",50,20).
-nota4("Sofia","Gestion de tecnologia","Parcial 2",42,20).
-nota4("Carmen","Gestion de tecnologia","Parcial 2",31,20).
-nota4("Charle","Gestion de tecnologia","Parcial 2",47,20).
-nota4("Manuel","Gestion de tecnologia","Parcial 2",21,20).
-nota4("Eduardo","Gestion de tecnologia","Parcial 2",11,20).
-nota4("Daniel","Gestion de tecnologia","Parcial 2",30,20).
+nota("Josue","Gestion de tecnologia","Parcial 2",35,20).
+nota("David","Gestion de tecnologia","Parcial 2",46,20).
+nota("Andres","Gestion de tecnologia","Parcial 2",32,20).
+nota("Liliana","Gestion de tecnologia","Parcial 2",50,20).
+nota("Sofia","Gestion de tecnologia","Parcial 2",42,20).
+nota("Carmen","Gestion de tecnologia","Parcial 2",31,20).
+nota("Charle","Gestion de tecnologia","Parcial 2",47,20).
+nota("Manuel","Gestion de tecnologia","Parcial 2",21,20).
+nota("Eduardo","Gestion de tecnologia","Parcial 2",11,20).
+nota("Daniel","Gestion de tecnologia","Parcial 2",30,20).
 
-nota5("Juan","Modelos 2","Final",30,20).
-nota5("Jorge","Modelos 2","Final",40,20).
-nota5("Carlos","Modelos 2","Final",25,20).
-nota5("Elkin","Modelos 2","Final",13,20).
-nota5("Goku","Modelos 2","Final",33,20).
-nota5("Jose","Modelos 2","Final",44,20).
-nota5("Nayibe","Modelos 2","Final",20,20).
-nota5("Yuyeimi","Modelos 2","Final",30,20).
-nota5("Yo","Modelos 2","Final",50,20).
-nota5("Lenon","Modelos 2","Final",45,20).
+nota("Juan","Modelos 2","Final",30,20).
+nota("Jorge","Modelos 2","Final",40,20).
+nota("Carlos","Modelos 2","Final",25,20).
+nota("Elkin","Modelos 2","Final",13,20).
+nota("Goku","Modelos 2","Final",33,20).
+nota("Jose","Modelos 2","Final",44,20).
+nota("Nayibe","Modelos 2","Final",20,20).
+nota("Yuyeimi","Modelos 2","Final",30,20).
+nota("Yo","Modelos 2","Final",50,20).
+nota("Lenon","Modelos 2","Final",45,20).
 
-nota5("Luis","Modelos 1","Final",24,25).
-nota5("Ricardo","Modelos 1","Final",43,25).
-nota5("Eddy","Modelos 1","Final",33,25).
-nota5("Ed","Modelos 1","Final",10,25).
-nota5("DD","Modelos 1","Final",50,25).
-nota5("Dexter","Modelos 1","Final",50,25).
-nota5("Timmy","Modelos 1","Final",29,25).
-nota5("Cosmo","Modelos 1","Final",15,25).
-nota5("Dave","Modelos 1","Final",22,25).
-nota5("Siri","Modelos 1","Final",49,25).
+nota("Luis","Modelos 1","Final",24,25).
+nota("Ricardo","Modelos 1","Final",43,25).
+nota("Eddy","Modelos 1","Final",33,25).
+nota("Ed","Modelos 1","Final",10,25).
+nota("DD","Modelos 1","Final",50,25).
+nota("Dexter","Modelos 1","Final",50,25).
+nota("Timmy","Modelos 1","Final",29,25).
+nota("Cosmo","Modelos 1","Final",15,25).
+nota("Dave","Modelos 1","Final",22,25).
+nota("Siri","Modelos 1","Final",49,25).
 
-nota5("Josue","Gestion de tecnologia","Final",35,20).
-nota5("David","Gestion de tecnologia","Final",46,20).
-nota5("Andres","Gestion de tecnologia","Final",32,20).
-nota5("Liliana","Gestion de tecnologia","Final",50,20).
-nota5("Sofia","Gestion de tecnologia","Final",42,20).
-nota5("Carmen","Gestion de tecnologia","Final",31,20).
-nota5("Charle","Gestion de tecnologia","Final",47,20).
-nota5("Manuel","Gestion de tecnologia","Final",21,20).
-nota5("Eduardo","Gestion de tecnologia","Final",11,20).
-nota5("Daniel","Gestion de tecnologia","Final",30,20).
+nota("Josue","Gestion de tecnologia","Final",35,20).
+nota("David","Gestion de tecnologia","Final",46,20).
+nota("Andres","Gestion de tecnologia","Final",32,20).
+nota("Liliana","Gestion de tecnologia","Final",50,20).
+nota("Sofia","Gestion de tecnologia","Final",42,20).
+nota("Carmen","Gestion de tecnologia","Final",31,20).
+nota("Charle","Gestion de tecnologia","Final",47,20).
+nota("Manuel","Gestion de tecnologia","Final",21,20).
+nota("Eduardo","Gestion de tecnologia","Final",11,20).
+nota("Daniel","Gestion de tecnologia","Final",30,20).
 
-definitiva(X,Y,N):-nota1(X,Y,H,S,Z),nota2(X,Y,J,R,W),nota3(X,Y,K,Q,V),nota4(X,Y,L,P,U),nota5(X,Y,M,O,T),N is (S*Z+R*W+Q*V+P*U+O*T)/100.
+definitiva(X,Y,N):-nota(X,Y,"Taller 1",S,Z),nota(X,Y,"Taller 2",R,W),nota(X,Y,"Parcial 1",Q,V),nota(X,Y,"Parcial 2",P,U),nota(X,Y,"Final",O,T),N is (S*Z+R*W+Q*V+P*U+O*T)/100.
 
 %pasa("Ricardo","Modelos 1").
 %pasa("Eddy","Modelos 1").
